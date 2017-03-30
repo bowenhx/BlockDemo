1183c7afe9af8d2beb313afc894d8145c7a66d4f
bowenhx <ligbhx@163.com>
2017-03-30 14:19:28 +0800

    Initial Commit

---
+++ BlockDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlockDemo
+//
+//  Created by ligb on 2017/3/30.
+//  Copyright © 2017年 com.mobile-kingdom.ekapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
